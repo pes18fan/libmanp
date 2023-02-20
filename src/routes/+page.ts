608e4e6e89c13c4905f41c2c8b20db93eea62c11
vezei <sadbhavadhikari@gmail.com>
2023-02-20 17:55:47 +0545

    [formatting]

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import { BaseDirectory, exists } from "@tauri-apps/api/fs";
 
 export async function load() {
-    return {
-        libFileExistence: await exists("lib.json", {dir: BaseDirectory.AppData}) // boolean denoting whether the lib.json file exists
-    };
+  return {
+    libFileExistence: await exists("lib.json", { dir: BaseDirectory.AppData }) // boolean denoting whether the lib.json file exists
+  };
 }
