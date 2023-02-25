6411c1d918e225c7c74ca86562e919c67fa6e1c8
vezei <sadbhavadhikari@gmail.com>
2023-02-25 21:11:08 +0545

    [feat] add modal ui for book add input, nonfunctional

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
+  // tauri stuff
   import { message } from "@tauri-apps/api/dialog";
   import { writeFile, BaseDirectory } from "@tauri-apps/api/fs";
   import { listen, emit } from "@tauri-apps/api/event";
 
   import type { PageData } from "./$types";
 
+  // for the modals
+  import { createModal } from "@grail-ui/svelte";
+  import { scale } from "svelte/transition";
+
+  const { useModal, modalAttrs, titleAttrs, open } = createModal({
+    portal: null,
+    dismissible: true,
+    keyboardDismissible: true
+  });
+
+  $open = false;
+
+  // components
   import BookActions from "$lib/BookActions.svelte";
   import Books from "$lib/Books.svelte";
 
+  // this data includes the lib.json file content loaded in every time the page reloads
   export let data: PageData;
 
   // listener to show info about the app when the About button in app menu is pressed
@@ -38,11 +53,29 @@
 
   // adds a new book to the file
   const processNewBook = () => {
-    emit("new-book", data.libData);
+    $open = true;
+
+    // emit("new-book", data.libData);
   };
 </script>
 
 <main>
+  {#if $open}
+    <div use:useModal transition:scale={{ duration: 300 }} {...$modalAttrs} class="addBookModal">
+      <h2 {...$titleAttrs}>Add a Book</h2>
+      <form class="addBookForm">
+        <label for="title">Name:</label> 
+        <input type="text" name="title" id="title" />
+        <br />
+        <label for="author">Author:</label> 
+        <input type="text" name="title" id="title" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+
+    <div id="overlay"></div>
+  {/if}
+
   <div class="container">
     <div class="sideInfo">
       <BookActions
