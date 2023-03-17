f8ed2f73595b66a204c97471596b7313902cd7d4
vezei <sadbhavadhikari@gmail.com>
2023-03-17 11:11:40 +0545

    [refactor] remove redundant +page.ts

--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import { BaseDirectory, exists, readTextFile } from "@tauri-apps/api/fs";
-
-export async function load() {
-  return {
-    fileData: (await exists("lib.json", { dir: BaseDirectory.AppData }))
-      ? await readTextFile("lib.json", { dir: BaseDirectory.AppData })
-      : "N/A"
-  };
-}
