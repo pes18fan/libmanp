c9390154e673480fb17bbadf58b78df61688c3cc
vezei <sadbhavadhikari@gmail.com>
2023-03-19 00:04:14 +0545

    [format]

--- src-tauri/src/add_book.rs
+++ src-tauri/src/add_book.rs
@@ -1,15 +1,15 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs::write;
 
 #[derive(Serialize, Deserialize)]
 struct Library {
-    books: Vec<Book>
+    books: Vec<Book>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Book {
     title: String,
-    author: String
+    author: String,
 }
 
 type AddBookResult<T> = std::result::Result<T, tauri::InvokeError>;
@@ -18,12 +18,15 @@ type AddBookResult<T> = std::result::Result<T, tauri::InvokeError>;
 pub fn add_new_book(existing_json: &str, file_path: &str, book_data: Book) -> AddBookResult<()> {
     // for some reason, the existing_json var ends up having extra backslashes and quotes and whatnot which mess it up and cause deserialization to fail
     // this line fixes that issue. thanks, ChatGPT!
-    let existing_json = existing_json.replace("\\", "").replace("\"{", "{").replace("}\"", "}");
+    let existing_json = existing_json
+        .replace("\\", "")
+        .replace("\"{", "{")
+        .replace("}\"", "}");
     let mut b: Library = serde_json::from_str(&existing_json).expect("Error parsing json");
 
-    b.books.push( Book {
+    b.books.push(Book {
         title: book_data.title,
-        author: book_data.author
+        author: book_data.author,
     });
 
     let j = serde_json::to_string(&b).expect("Error converting struct to json");
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod add_book;
@@ -13,34 +13,34 @@ use crate::watch::watch;
 
 // creates the main menu
 pub fn create_menu() -> Menu {
-  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
 
-  let about = CustomMenuItem::new("about".to_string(), "About");
+    let about = CustomMenuItem::new("about".to_string(), "About");
 
-  let file = Submenu::new("File", Menu::new().add_item(quit));
-  let help = Submenu::new("Help", Menu::new().add_item(about));
+    let file = Submenu::new("File", Menu::new().add_item(quit));
+    let help = Submenu::new("Help", Menu::new().add_item(about));
 
-  Menu::new()
-    .add_native_item(MenuItem::Copy)
-    .add_submenu(file)
-    .add_submenu(help)
+    Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_submenu(file)
+        .add_submenu(help)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .menu(create_menu())
-    // event handler for when the various menu buttons are pressed
-    .on_menu_event(|event| match event.menu_item_id() {
-      "quit" => {
-        std::process::exit(0);
-      }
-      "about" => {
-        event.window().emit("about", "").unwrap();
-      }
-      _ => {}
-    })
-    .invoke_handler(tauri::generate_handler![add_new_book, watch])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .menu(create_menu())
+        // event handler for when the various menu buttons are pressed
+        .on_menu_event(|event| match event.menu_item_id() {
+            "quit" => {
+                std::process::exit(0);
+            }
+            "about" => {
+                event.window().emit("about", "").unwrap();
+            }
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![add_new_book, watch])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
