a3ec12032e097165fc97f77619dc8a7f3939d6c3
pes18fan <pes18fan@disr.it>
2023-03-26 16:19:04 +0545

    [feat] add ascending sort by title and author

--- src/lib/Books.svelte
+++ src/lib/Books.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
+  import { createMenu } from "@grail-ui/svelte";
+  import { fade } from "svelte/transition";
+
+  const sortOptions = [
+    { id: "alphabeticallyTitle", label: "Alphabetically (Title)" },
+    { id: "alphabeticallyAuthor", label: "Alphabetically (Author)" }
+  ];
+
+  // for the sort menu
+  const { useTrigger, triggerAttrs, useMenu, menuAttrs, itemAttrs, open } =
+    createMenu({
+      positioning: {
+        strategy: "fixed",
+        placement: "bottom"
+      },
+      onSelect(id) {
+        $open = false;
+
+        switch (id) {
+          case "alphabeticallyTitle":
+            sortBooksAlphabeticallyAscendingTitle();
+            break;
+          case "alphabeticallyAuthor":
+            sortBooksAlphabeticallyAscendingAuthor();
+            break;
+        }
+      }
+    });
+
   export let bookList: Array<Book> = [];
   export let handleBookSelect: (book: Book) => void;
 
   let searchBox: HTMLInputElement;
   let bookListElement: HTMLUListElement;
 
+  // function to search through the book list
   const searchBooks = () => {
     if (bookList.length === 0) return;
 
@@ -23,29 +53,115 @@
       }
     }
   };
+
+  const sortBooksAlphabeticallyAscendingTitle = () => {
+    if (bookList.length === 0) return;
+
+    const bookElements = bookListElement.getElementsByTagName("li");
+    let switching = true;
+    let shouldSwitch = false;
+    let i: number;
+
+    while (switching) {
+      switching = false;
+
+      for (i = 0; i < bookElements.length - 1; i++) {
+        shouldSwitch = false;
+
+        if (
+          bookElements[i].dataset.title!.toUpperCase() >
+          bookElements[i + 1].dataset.title!.toUpperCase()
+        ) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+
+      if (shouldSwitch) {
+        bookElements[i].parentNode?.insertBefore(
+          bookElements[i + 1],
+          bookElements[i]
+        );
+        switching = true;
+      }
+    }
+  };
+
+  const sortBooksAlphabeticallyAscendingAuthor = () => {
+    if (bookList.length === 0) return;
+
+    const bookElements = bookListElement.getElementsByTagName("li");
+    let switching = true;
+    let shouldSwitch = false;
+    let i: number;
+
+    while (switching) {
+      switching = false;
+
+      for (i = 0; i < bookElements.length - 1; i++) {
+        shouldSwitch = false;
+
+        if (
+          bookElements[i].dataset.author!.toUpperCase() >
+          bookElements[i + 1].dataset.author!.toUpperCase()
+        ) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+
+      if (shouldSwitch) {
+        bookElements[i].parentNode?.insertBefore(
+          bookElements[i + 1],
+          bookElements[i]
+        );
+        switching = true;
+      }
+    }
+  };
 </script>
 
 <div class="books">
   <h1>Books</h1>
   {#if bookList.length !== 0}
-    <input
-      bind:this={searchBox}
-      placeholder="Look for a book..."
-      on:keyup={searchBooks}
-    />
+    <div class="book-list-actions">
+      <input
+        bind:this={searchBox}
+        placeholder="Look for a book..."
+        on:keyup={searchBooks}
+      />
+      <button class="sort-button" use:useTrigger {...$triggerAttrs}>Sort</button>
+
+      {#if $open}
+        <ul transition:fade use:useMenu {...$menuAttrs} class="sort-options">
+          {#each sortOptions as sortOption (sortOption.id)}
+            <li class="sort-option">
+              <a
+                class="sort-option-anchor"
+                href="/"
+                {...$itemAttrs(sortOption.id)}>{sortOption.label}</a
+              >
+            </li>
+          {/each}
+        </ul>
+      {/if}
+    </div>
+
     <ul bind:this={bookListElement}>
       {#each bookList as book}
-        <li>
+        <li data-title={book.title} data-author={book.author}>
           <div
             class="book"
             on:click={() => handleBookSelect(book)}
             on:keydown={() => handleBookSelect(book)}
           >
-          {book.title}
+            {book.title}
             <br />
             <span class="label">Author: </span>{book.author}
             <br />
-            <span class="label">Added: </span>{new Date(book.date_added).toLocaleDateString()}
+            <span class="label">Added: </span>{new Date(
+              book.date_added
+            ).toLocaleDateString()}
           </div>
         </li>
       {/each}
@@ -70,8 +186,45 @@
       margin: 10px;
     }
 
-    input {
-      @include inputStyles;
+    .book-list-actions {
+      display: flex;
+      width: 90%;
+
+      input {
+        @include inputStyles;
+      }
+
+      .sort-button {
+        font-size: 18px;
+        width: 20%;
+        padding: 5px;
+      }
+
+      .sort-options {
+        display: flex;
+        flex-direction: column;
+        background: $primary-color;
+        width: auto;
+        padding: 10px;
+        border-radius: 5px;
+        border: 1px solid $accent-color;
+
+        .sort-option {
+          margin: 10px 0px 10px 0px;
+
+          .sort-option-anchor {
+            transition: all 300ms ease;
+            padding: 10px;
+            border-radius: 10px;
+            text-decoration: none;
+            color: #ffffffde;
+
+            &:hover {
+              background-color: $secondary-color;
+            }
+          }
+        }
+      }
     }
 
     ul {
